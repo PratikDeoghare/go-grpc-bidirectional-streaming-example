ccc70e66aff548ca939c581065be87d41d61194c
Pratik Deoghare <pratik.deoghare@gmail.com>
2022-07-07 14:19:26 +0200

    call Send concurrently

--- src/client/client.go
+++ src/client/client.go
@@ -5,52 +5,55 @@ import (
 	"io"
 	"log"
 	"math/rand"
-
+	
 	pb "github.com/pahanini/go-grpc-bidirectional-streaming-example/src/proto"
-
+	
 	"time"
-
+	
 	"google.golang.org/grpc"
 )
 
 func main() {
 	rand.Seed(time.Now().Unix())
-
+	
 	// dail server
 	conn, err := grpc.Dial(":50005", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("can not connect with server %v", err)
 	}
-
+	
 	// create stream
 	client := pb.NewMathClient(conn)
 	stream, err := client.Max(context.Background())
 	if err != nil {
 		log.Fatalf("openn stream error %v", err)
 	}
-
+	
 	var max int32
 	ctx := stream.Context()
 	done := make(chan bool)
-
+	
 	// first goroutine sends random increasing numbers to stream
 	// and closes it after 10 iterations
-	go func() {
-		for i := 1; i <= 10; i++ {
-			// generates random number and sends it to stream
-			rnd := int32(rand.Intn(i))
-			req := pb.Request{Num: rnd}
-			if err := stream.Send(&req); err != nil {
-				log.Fatalf("can not send %v", err)
+	
+	for i := 0; i < 1000; i++ {
+		go func() {
+			for i := 1; i <= 10000; i++ {
+				// generates random number and sends it to stream
+				rnd := int32(rand.Intn(i))
+				req := pb.Request{Num: rnd}
+				if err := stream.Send(&req); err != nil {
+					log.Fatalf("can not send %v", err)
+				}
+				log.Printf("%d sent", req.Num)
+				//time.Sleep(time.Millisecond * 200)
 			}
-			log.Printf("%d sent", req.Num)
-			time.Sleep(time.Millisecond * 200)
-		}
-		if err := stream.CloseSend(); err != nil {
-			log.Println(err)
-		}
-	}()
-
+			if err := stream.CloseSend(); err != nil {
+				log.Println(err)
+			}
+		}()
+	}
+	
 	// second goroutine receives data from stream
 	// and saves result in max variable
 	//
@@ -69,7 +72,7 @@ func main() {
 			log.Printf("new max %d received", max)
 		}
 	}()
-
+	
 	// third goroutine closes done channel
 	// if context is done
 	go func() {
@@ -79,7 +82,7 @@ func main() {
 		}
 		close(done)
 	}()
-
+	
 	<-done
 	log.Printf("finished with max=%d", max)
 }
